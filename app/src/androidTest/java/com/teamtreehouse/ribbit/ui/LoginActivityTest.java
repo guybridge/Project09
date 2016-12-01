3945a039e1a5887f4cbb1a1dde7c25f5cd94a4bf
Guy Bridge <guy@wsit.com.au>
2016-12-01 17:16:21 +0800

    All tests passing in Espresso

--- app/src/androidTest/java/com/teamtreehouse/ribbit/ui/LoginActivityTest.java
+++ app/src/androidTest/java/com/teamtreehouse/ribbit/ui/LoginActivityTest.java
@@ -9,12 +9,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static java.util.EnumSet.allOf;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
 
 /**
@@ -34,10 +37,10 @@ public class LoginActivityTest
         onView(withId(R.id.usernameField)).perform(typeText(username));
 
         // Act
-        onView(withId(R.id.loginButton)).perform(pressImeActionButton());
+        onView(withId(R.id.loginButton)).perform(click());
 
         // Assert - title id should still be there because activity should not go to main
-        onView(withText(R.id.title)).check(matches(notNullValue()));
+        onView(withText(android.R.string.ok)).check(matches(notNullValue()));
 
     }
 
@@ -49,10 +52,10 @@ public class LoginActivityTest
         onView(withId(R.id.passwordField)).perform(typeText(password));
 
         // Act
-        onView(withId(R.id.loginButton)).perform(pressImeActionButton());
+        onView(withId(R.id.loginButton)).perform(click());
 
         // Assert - title id should still be there because activity should not go to main
-        onView(withText(R.id.title)).check(matches(notNullValue()));
+        onView(withText(android.R.string.ok)).check(matches(notNullValue()));
 
     }
 
@@ -61,12 +64,29 @@ public class LoginActivityTest
     {
         // Arrange
 
-
         // Act
-        onView(withId(R.id.loginButton)).perform(pressImeActionButton());
+        onView(withId(R.id.loginButton)).perform(click());
 
         // Assert - title id should still be there because activity should not go to main
-        onView(withText(R.id.title)).check(matches(notNullValue()));
+        onView(withText(android.R.string.ok)).check(matches(notNullValue()));
+
+    }
+
+    @Test
+    public void loginWorks() throws Exception
+    {
+        // Arrange
+        String username = "Daniel";
+        String password = "TESTpass01";
+        onView(withId(R.id.usernameField)).perform(typeText(username));
+        onView(withId(R.id.passwordField)).perform(typeText(password));
+
+        // Act
+        onView(withId(R.id.loginButton)).perform(click());
+        Thread.sleep(3000);
+
+        // Assert
+        onView(withId(R.id.pager)).check(matches(notNullValue()));
 
     }
 }
